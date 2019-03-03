f25c0507cdc8e605f297ff8558673545efb7d081
Thomas Joußen <tjoussen@gmx.de>
2019-03-03 22:53:30 +0100

    Update documentation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 /**
  * Create an action.
  *
- * @param type
- * @param argNames
- * @returns {function(...[*]): {type: *}}
+ * @example <caption>Create an empty action</caption>
+ * const HEARTBEAT = 'heartbeat'
+ * const heartbeat = createAction(HEARTBEAT)
+ * const reduxAction = heartbeat()
+ *
+ * console.log(reduxAction); // { type: 'heartbeat' }
+ *
+ * @example <caption>Action with at least one parameter</caption>
+ * const ADD_POST = 'post/add'
+ * const addPost = createAction(ADD_POST, 'title')
+ * const reduxAction = addPost('New post')
+ *
+ * console.log(reduxAction); // { type: 'post/add', title: 'New post'}
+ *
+ * @param {string}      type     - The actions type.
+ * @param {...[string]} argNames - The actions argument names.
+ * @returns {function(...args): {type:string}}
  */
 export const createAction = (type, ...argNames) => {
     return (...args) => {
@@ -20,9 +34,16 @@ export const createAction = (type, ...argNames) => {
 /**
  * Create request, success and failure type for async actions.
  *
+ * @example <caption>Create an array of async action types</caption>
+ * const [REQUEST_TYPE, SUCCESS_TYPE, FAILURE_TYPE] = createAsyncActionTypes('call/api')
+ *
+ * console.log(REQUEST_TYPE); // call/api/request
+ * console.log(SUCCESS_TYPE); // call/api/success
+ * console.log(FAILURE_TYPE); // call/api/failure
+ *
  * @param {string} type - The type base name
  * @param {string} delimiter - A delimiter string
- * @returns {string[]}
+ * @returns {Array<string>}
  */
 export const createAsyncActionTypes = (type, delimiter = '/') => {
     return [
@@ -39,7 +60,7 @@ export const createAsyncActionTypes = (type, delimiter = '/') => {
  * @param {Function} promise           - The async action to be executed
  * @param {Function} shouldCallPromise - Function to determine if the promise should be executed.
  * @param {...*}     argNames          - List of all property names which will passed as payload.
- * @returns {function(...[*]): {types: *, payload: {}, shouldCallPromise: (function(): boolean), promise: *}}
+ * @returns {function(...[*]): {types: *, payload: {}, shouldCallPromise: (function(): boolean), promise: function}}
  */
 export const createAsyncAction = (types, promise, shouldCallPromise = () => true, ...argNames) => {
     const actionTemplate = { types, promise, shouldCallPromise, payload: {} };
@@ -57,6 +78,12 @@ export const createAsyncAction = (types, promise, shouldCallPromise = () => true
     };
 };
 
+/**
+ * Create a middleware
+ *
+ * @param {object} handlers - Handlers which will be triggered by an action.
+ * @returns {function({dispatch?: *, getState?: *}): function(*): Function}
+ */
 export const createMiddleware = (handlers) => {
     return ({dispatch, getState}) => {
         return next => action => {
@@ -69,6 +96,10 @@ export const createMiddleware = (handlers) => {
     };
 };
 
+/**
+ * Create the middleware for async actions.
+ * @returns {function({dispatch?: *, getState?: *}): function(*): Function}
+ */
 export const createAsyncMiddleware = () => {
     return ({dispatch, getState}) => {
         return next => action => {
@@ -90,6 +121,11 @@ export const createAsyncMiddleware = () => {
     };
 };
 
+/**
+ *
+ * @param {object} action - The async action
+ * @private
+ */
 export const throwErrorOnInvalidAsyncAction = (action) => {
     const { types, promise, shouldCallPromise, payload } = action;
 
@@ -114,6 +150,12 @@ export const throwErrorOnInvalidAsyncAction = (action) => {
     }
 };
 
+/**
+ * Call an actions promise and dispatch the sub actions.
+ * @param {function} dispatch
+ * @param {object}   action
+ * @returns {Promise}
+ */
 export const callPromise = (dispatch, action) => {
     const { types, payload, promise } = action;
     const [ requestType, successType, failureType ] = types;
