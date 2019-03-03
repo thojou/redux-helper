33fa35ac64a04e457e92ccfd8c1589715976283f
Thomas Joußen <tjoussen@gmx.de>
2019-03-03 20:33:17 +0100

    Implement createAsyncMiddleware

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+
+
+
 export const createAction = (type, ...argNames) => {
     return (...args) => {
         let action = { type };
@@ -54,3 +57,65 @@ export const createMiddleware = (handlers) => {
         };
     };
 };
+
+export const createAsyncMiddleware = () => {
+    return ({dispatch, getState}) => {
+        return next => action => {
+
+            if (!action.hasOwnProperty('types')) {
+                return next(action);
+            }
+
+            throwErrorOnInvalidAsyncAction(action);
+
+            if(!action.shouldCallPromise(getState())) {
+                return next(action);
+            }
+
+            callPromise(dispatch, action);
+
+            return next(action);
+        };
+    };
+};
+
+export const throwErrorOnInvalidAsyncAction = (action) => {
+    const { types, promise, shouldCallPromise, payload } = action;
+
+    if (
+        !Array.isArray(types) ||
+        types.length !== 3 ||
+        !types.every(type => typeof type === 'string')
+    ) {
+        throw new Error('Expected an array of three string types.');
+    }
+
+    if (typeof promise !== 'function') {
+        throw new Error('The parameter `promise` is expected to be a function');
+    }
+
+    if (typeof shouldCallPromise !== 'function') {
+        throw new Error('The parameter `shouldCallPromise` is expected to be a function');
+    }
+
+    if (typeof payload !== 'object') {
+        throw new Error('The parameter `payload` is expected to be an object');
+    }
+};
+
+export const callPromise = (dispatch, action) => {
+    const { types, payload, promise } = action;
+    const [ requestType, successType, failureType ] = types;
+
+    dispatch({type: requestType, ...payload});
+
+    return promise(...Object.values(payload))
+        .then((response) => {
+            dispatch({type: successType, ...payload, response});
+        })
+        .catch((error) => {
+            dispatch({type: failureType, ...payload, error});
+        });
+};
+
+
